4e8a23f897b3c89b761e4e98367751b1a679c91b
Nishant Srivastava <20250366+nishi7409@users.noreply.github.com>
2020-10-05 01:51:47 +0530

    Update models.py

--- ethicssite/survey/models.py
+++ ethicssite/survey/models.py
@@ -2,6 +2,8 @@ import datetime
 from django.db import models
 
 # Question model
+
+
 class questionModel(models.Model):
 	# Question field (text field shown to user)
 	question_txt: models.CharField(max_length=200, null=False)
@@ -19,21 +21,29 @@ class questionModel(models.Model):
 	date = models.DateField(("Date"), default=datetime.date.today)
 
 	# Implement options when the options object is created above ^^
-    @classmethod
-    def create(cls, questionTXT, questionDESC, optionsTXT):
-    	# I don't think I need to pass in a parameter for the date 
-        questionObject = cls(question_txt = questionTXT, question_desc = questionDESC, options = optionsTXT, date = datetime.date.today)
-        return(questionObject)
-        
+    # @classmethod
+    # def create(cls, questionTXT, questionDESC, optionsTXT):
+    # 	# I don't think I need to pass in a parameter for the date
+    #     questionObject = cls(question_txt = questionTXT, question_desc = questionDESC, options = optionsTXT, date = datetime.date.today)
+    #     return(questionObject)
+
 	# Print the question if invoked
 	def __str__(self):
 		return self.question_txt
 
+	"""
+	class DummyModel(models.Model):
+		question_text = models.CharField(max_length=200)
+		pub_date = models.DateTimeField('date published')
+
+		@classmethod
+		def create(cls,text=None):
+			obj = cls(question_text = text)
+			return obj
+	"""
+
+
 # Model for user settings
-class userSettings(models.Model):
-	# generator for a set of scenarios (linked to the user settings)
-	return(None)
+# generator for a set of scenarios (linked to the user settings)
 
 # Model for storing user input scores
-class inputScores(models.Model):
-	return(None)
