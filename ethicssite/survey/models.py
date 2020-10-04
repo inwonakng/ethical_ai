c1422deebd937cab7cf6b9c77d96af12fe621fd6
Nishant Srivastava <20250366+nishi7409@users.noreply.github.com>
2020-10-05 02:57:27 +0530

    Update models.py

--- ethicssite/survey/models.py
+++ ethicssite/survey/models.py
@@ -2,8 +2,7 @@ import datetime
 from django.db import models
 
 # Question model
-
-
+@python_2_unicode_compatible
 class questionModel(models.Model):
 	# Question field (text field shown to user)
 	question_txt: models.CharField(max_length=200, null=False)
@@ -12,35 +11,45 @@ class questionModel(models.Model):
 	question_desc: models.TextField(null=False, blank=False)
 
 	# Not including images (like OPRA) because what happens if the image is
-	# inappropriate (since we're having literally anyone have access to creating)?
-
-	# minimum of 2 options that must be entered which can dynamically grow
-	# options = models.Array || WHAT DATABASE??
+	# inappropriate (since we're having anyone have access to creating)?
 
 	# Date when question was submitted (auto done in backend)
-	date = models.DateField(("Date"), default=datetime.date.today)
+	date = models.DateTimeField(default=datetime.date.today)
 
-	# Implement options when the options object is created above ^^
-    # @classmethod
-    # def create(cls, questionTXT, questionDESC, optionsTXT):
-    # 	# I don't think I need to pass in a parameter for the date
-    #     questionObject = cls(question_txt = questionTXT, question_desc = questionDESC, options = optionsTXT, date = datetime.date.today)
-    #     return(questionObject)
-
-	# Print the question if invoked
+	# Return question text
 	def __str__(self):
 		return self.question_txt
 
-	"""
-	class DummyModel(models.Model):
-		question_text = models.CharField(max_length=200)
-		pub_date = models.DateTimeField('date published')
+	@classmethod
+	def create(cls, questionTXT, questionDESC):
+	    questionObject = cls(question_txt = questionTXT, question_desc = questionDESC)
+	    return(questionObject)
 
-		@classmethod
-		def create(cls,text=None):
-			obj = cls(question_text = text)
-			return obj
-	"""
+# Option/choice model (from OPRA)
+@python_2_unicode_compatible
+class option(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    option_txt = models.CharField(max_length=200)
+    timestamp = models.DateTimeField('item timestamp')
+
+	# Return option text
+    def __str__(self):
+        return self.option_txt
+
+	# ordering
+    class Meta:
+        ordering = ['timestamp']
+
+"""
+class DummyModel(models.Model):
+	question_text = models.CharField(max_length=200)
+	pub_date = models.DateTimeField('date published')
+
+	@classmethod
+	def create(cls,text=None):
+		obj = cls(question_text = text)
+		return obj
+"""
 
 
 # Model for user settings
