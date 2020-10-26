9d831b28ebbf47b0c6d0fc406fb70453bb2471af
Mitesh Kumar <miteshkumarca@gmail.com>
2020-10-25 22:50:42 -0500

    update tests

--- ethicssite/survey/tests.py
+++ ethicssite/survey/tests.py
@@ -10,24 +10,24 @@ from survey.models import RuleSet, ChoiceCategory, RuleSetChoice, RangeCategory,
 script_dir = os.path.dirname(__file__)
 
 
-
-
 class UserSettingTestCase(TestCase):
-    
+
     def test_to_string(self):
         with open(os.path.join(script_dir, 'rule_test.json')) as f:
             data = json.load(f)
 
         orig_json_string = json.dumps(data, indent=4)
         new_rule_set = create_rule_set_from_json_string(orig_json_string)
-        
-        self.assertEqual(json.dumps(new_rule_set.object_form(), indent=4), orig_json_string)
-        
+
+        self.assertEqual(json.dumps(
+            new_rule_set.object_form(), indent=4), orig_json_string)
+
         loaded_rule_set = RuleSet.objects.get(pk=new_rule_set.id)
 
-        self.assertEqual(json.dumps(loaded_rule_set.object_form(), indent=4), orig_json_string)
-        self.assertEqual(json.dumps(loaded_rule_set.object_form(), indent=4), json.dumps(new_rule_set.object_form(), indent=4))
-
+        self.assertEqual(json.dumps(
+            loaded_rule_set.object_form(), indent=4), orig_json_string)
+        self.assertEqual(json.dumps(loaded_rule_set.object_form(
+        ), indent=4), json.dumps(new_rule_set.object_form(), indent=4))
 
     def test_object_form(self):
 
@@ -44,10 +44,14 @@ class UserSettingTestCase(TestCase):
 
         choice_gender = ChoiceCategory(name="gender")
         rule_set.choicecategory_set.add(choice_gender, bulk=False)
-        choice_gender.rulesetchoice_set.create(index=0, description="in great health")
-        choice_gender.rulesetchoice_set.create(index=1, description="small health problems")
-        choice_gender.rulesetchoice_set.create(index=2, description="moderate health problems")
-        choice_gender.rulesetchoice_set.create(index=3, description="terminally ill(less than 3 years left)")
+        choice_gender.rulesetchoice_set.create(
+            index=0, description="in great health")
+        choice_gender.rulesetchoice_set.create(
+            index=1, description="small health problems")
+        choice_gender.rulesetchoice_set.create(
+            index=2, description="moderate health problems")
+        choice_gender.rulesetchoice_set.create(
+            index=3, description="terminally ill(less than 3 years left)")
 
         range_survival_without = RangeCategory(
             name="survival without",
@@ -56,12 +60,7 @@ class UserSettingTestCase(TestCase):
             unit="%")
         rule_set.rangecategory_set.add(range_survival_without, bulk=False)
 
-
         # print(json.dumps(rule_set.object_form(), indent=4))
 
 
-
-
-
 # Create your tests here.
-
