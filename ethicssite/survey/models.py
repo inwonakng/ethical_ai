7104bcdd4b2d369decc5e2bee406f66bf390e584
josephom12 <joeom516@gmail.com>
2020-10-31 17:18:45 -0400

    Add attributes to Survey model

--- ethicssite/survey/models.py
+++ ethicssite/survey/models.py
@@ -10,6 +10,8 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 # Fields:
 #     name     : CharField
 #     value    : CharField
+
+
 class Attribute(models.Model):
     # attribute name (e.g. age or health)
     name = models.CharField(max_length=50, null=False, default='')
@@ -26,7 +28,8 @@ class Option(models.Model):
     # name of current Combo (e.g. Person A)
     name = models.CharField(max_length=50, null=False, default='')
     # attributes under the current Combo
-    attributes = models.ManyToManyField(Attribute, related_name='combo_attributes')
+    attributes = models.ManyToManyField(
+        Attribute, related_name='combo_attributes')
 
 
 # Model for a scenario
@@ -37,28 +40,31 @@ class Scenario(models.Model):
     options = models.ManyToManyField(Option)
 
 
-
 # Model for a survey (mother of all models)
 
-# Fields: 
+# Fields:
 #     prompt    : CharField
 #     desc      : TextField
 #     date      : DateTimeField (optional)
-#     scenarios : ManyToMany of model Scenario 
+#     scenarios : ManyToMany of model Scenario
 class Survey(models.Model):
     # Survey field (text field shown to user)
-    prompt = models.CharField(max_length=200, null=False,default='')
+    prompt = models.CharField(max_length=200, null=False, default='')
 
     # Survey description (text field shown to user)
-    desc = models.TextField(null=False, blank=False,default='')
+    desc = models.TextField(null=False, blank=False, default='')
 
     # Date when question was submitted (auto done in backend)
     date = models.DateTimeField(default=timezone.now)
     scenarios = models.ManyToManyField(Scenario)
+    attributes = models.ManyToManyField(Attribute)
+
 
 '''Survey models sections end here'''
 
 # Model for storing user input scores
+
+
 class Response(models.Model):
     # consider including some fields for identifying the user?
     # userid = ?
@@ -71,13 +77,16 @@ class Response(models.Model):
     # scores = [models.IntegerField() for o in survey..scenarios]
 
 # Rule model
+
+
 class RuleSet(models.Model):
     choice_categs = models.ManyToManyField('ListCateg')
     range_categs = models.ManyToManyField('RangeCateg')
     badcombos = models.ManyToManyField('BadCombo')
-    same_categories = models.IntegerField(null=False,default=2)
-    scenario_size = models.IntegerField(null=False,default=2)
+    same_categories = models.IntegerField(null=False, default=2)
+    scenario_size = models.IntegerField(null=False, default=2)
     '''These accessor functions are for the generator to use'''
+
     def get_choicecategs(self):
         # for cc in self.choice_categs
         categ = {}
@@ -92,7 +101,7 @@ class RuleSet(models.Model):
         ran = self.get_rangecategs()
         cho.update(ran)
         return {
-            'config':self.get_configs(),
+            'config': self.get_configs(),
             'categories': cho,
             'bad_combos': self.get_badcombos()
         }
@@ -102,21 +111,24 @@ class RuleSet(models.Model):
         for cc in self.range_categs.all():
             bb.update(cc.object_form())
         return bb
-            
+
     def get_badcombos(self):
-        return {bc.object_form()[0]:bc.object_form()[1] 
-        for bc in self.badcombos.all()}
-        
+        return {bc.object_form()[0]: bc.object_form()[1]
+                for bc in self.badcombos.all()}
+
     def get_configs(self):
         return {
-            'same_categories':self.same_categories,
+            'same_categories': self.same_categories,
             'scenario_size': self.scenario_size
         }
 
 # Bad combination model
+
+
 class BadCombo(models.Model):
     category_name = models.CharField(max_length=100)
     subcombos = models.ManyToManyField('BadSubCombo')
+
     def object_form(self):
         return (self.category_name, {key: value for (key, value) in [obj.object_form() for obj in self.subcombos.all()]})
 
@@ -124,22 +136,28 @@ class BadCombo(models.Model):
         return self.category_name
 
 # This level contains the bad combos for specified category
+
+
 class BadSubCombo(models.Model):
     categ = models.CharField(max_length=500)
     badcombo_elems = models.ManyToManyField('BadSubComboElement')
+
     def object_form(self):
-        return (self.categ, 
-                {key: value 
-                for (key, value) in 
+        return (self.categ,
+                {key: value
+                 for (key, value) in
                     [obj.object_form() for obj in self.badcombo_elems.all()]})
-        
+
     def __str__(self):
         return self.categ
 
 # Bad sub?? combination element model
+
+
 class BadSubComboElement(models.Model):
     categ = models.CharField(max_length=100)
     elems = models.ManyToManyField('ElementChoice')
+
     def object_form(self):
         return (self.categ, [str(elem_choice) for elem_choice in self.elems.all()])
 
@@ -147,24 +165,32 @@ class BadSubComboElement(models.Model):
         return self.categ
 
 # Element choice model
+
+
 class ElementChoice(models.Model):
     category_index = models.IntegerField()
+
     def __str__(self):
         return str(self.category_index)
 
 # Choice category model - choosing froma list of values
+
+
 class ListCateg(models.Model):
     name = models.CharField(max_length=100)
     choices = models.ManyToManyField('RuleSetChoice')
+
     def object_form(self):
         r = {c.object_form()[0]: c.object_form()[1]
-            for c in self.choices.all()}
-        return [self.name,r]
+             for c in self.choices.all()}
+        return [self.name, r]
 
     def __str__(self):
         return json.dumps(self.object_form())
 
 # Rule set choice model
+
+
 class RuleSetChoice(models.Model):
     index = models.IntegerField()
     value = models.CharField(max_length=500)
@@ -176,6 +202,8 @@ class RuleSetChoice(models.Model):
         return json.dumps({str(self.index): self.value})
 
 # Range category model
+
+
 class RangeCateg(models.Model):
     name = models.CharField(max_length=100)
     minVal = models.FloatField()
@@ -190,6 +218,7 @@ class RangeCateg(models.Model):
 
     def __str__(self):
         return json.dumps(self.object_form())
+
 
 '''
 Currently this function receives the survey data as in a format of [Scenario1,scenario2,...].
@@ -269,8 +298,10 @@ Test scenario example:
 '''
 # This function will recieve a list of json scenarios and
 # load them into Django models.
+
+
 def json_to_survey(survey_data, prompt='empty', desc='empty'):
-    
+
     curr_survey = Survey(prompt=prompt, desc=desc)
     curr_survey.save()
 
@@ -290,12 +321,15 @@ def json_to_survey(survey_data, prompt='empty', desc='empty'):
 
             for attribute in option:
                 value = option[attribute]
-                
+
                 curr_attribute = Attribute(name=attribute, value=value)
                 curr_attribute.save()
 
                 curr_option.attributes.add(curr_attribute)
                 curr_option.save()
+
+                curr_survey.attributes.add(curr_attribute)
+                curr_survey.save()
 
             curr_scenario.options.add(curr_option)
             curr_scenario.save()
@@ -304,15 +338,15 @@ def json_to_survey(survey_data, prompt='empty', desc='empty'):
         curr_survey.save()
 
     return curr_survey
-    
+
 
 # type(d) must be dict()
 def json_to_ruleset(d):
     inp = {'same_categories': d['config']['same_categories'],
-    'scenario_size': d['config']['scenario_size']}
+           'scenario_size': d['config']['scenario_size']}
     rule_set = RuleSet(**inp)
     rule_set.save()
-    for categ,obj in d['categories'].items():
+    for categ, obj in d['categories'].items():
         if 'range' in obj:
             range_category = RangeCateg(
                 name=categ,
@@ -324,15 +358,15 @@ def json_to_ruleset(d):
         else:
             list_categ = ListCateg(name=categ)
             list_categ.save()
-            for k,v in obj.items():
+            for k, v in obj.items():
                 list_categ.choices.create(
                     index=k, value=v)
             rule_set.choice_categs.add(list_categ)
 
-    for categ_name,obj in d['bad_combos'].items():
+    for categ_name, obj in d['bad_combos'].items():
         bad_combo = BadCombo(category_name=categ_name)
         bad_combo.save()
-        for cval,sub_obj in obj.items():
+        for cval, sub_obj in obj.items():
             subcombo = BadSubCombo(
                 categ=cval)
             subcombo.save()
@@ -344,10 +378,10 @@ def json_to_ruleset(d):
                 for category_index in sub_obj[category_name_in]:
                     bsubcom_elem.elems.create(
                         category_index=category_index)
-                
+
                 subcombo.badcombo_elems.add(
                     bsubcom_elem)
-        
+
             bad_combo.subcombos.add(
                 subcombo)
         rule_set.badcombos.add(bad_combo)
