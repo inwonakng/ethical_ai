a83163ad29c4c99964ae9d79e7a3d8905d3aa352
inwonakng <inwon.kang04@gmail.com>
2020-11-23 00:24:25 -0500

    email login

--- ethicssite/ethicssite/settings.py
+++ ethicssite/ethicssite/settings.py
@@ -138,8 +138,8 @@ USE_TZ = True
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = ''            # Host email (example@gmail.com)
-EMAIL_HOST_PASSWORD = ''        # Host email password
+EMAIL_HOST_USER = 'ethical.surveys@gmail.com'            # Host email (example@gmail.com)
+EMAIL_HOST_PASSWORD = 'uXzjJADesucm5dF'        # Host email password
 EMAIL_PORT = 587
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
