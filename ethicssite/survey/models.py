042d5d6314c5fc76b2dd33389d250b79f5a4fc9a
inwonakng <inwon.kang04@gmail.com>
2020-12-01 18:56:04 -0500

    id stored as int

--- ethicssite/survey/models.py
+++ ethicssite/survey/models.py
@@ -158,15 +158,11 @@ class Option(models.Model):
 class SingleResponse(models.Model):
     value = models.CharField(max_length=50, null=False, default='')
 
-class Attribute(models.Model):
-    name = models.CharField(max_length=50, null=False, default='')
-    value = models.CharField(max_length=50, null=False, default='')
-
 # Holds ruleset ID and scenario model
 class TempScenarios(models.Model):
-    user_id = models.CharField(null=False, blank=False)
-    session_id = models.CharField(null=False, blank=False)
-    ruleset_id = models.CharField(null=False, blank=False)
+    user_id = models.IntegerField(null=False, blank=False)
+    session_id = models.IntegerField(null=False, blank=False)
+    ruleset_id = models.IntegerField(null=False, blank=False)
     scenario = models.ManyToManyField('Scenario')
 
 
